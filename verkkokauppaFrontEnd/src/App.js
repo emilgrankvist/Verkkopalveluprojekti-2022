630657788ff740afd6a1f0c5aef683912bf2a169
Aktonen <aku.tolonen1999@hotmail.fi>
2022-03-20 12:14:51 +0200

    Sivun protoa

--- verkkokauppaFrontEnd/src/App.js
+++ verkkokauppaFrontEnd/src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Tietokonekauppa</h1>
+      <div>
+        <header><a>Tietokoneet</a><a>Komponentit</a><a>Oheislaitteet</a></header>
+      </div>
     </div>
+
   );
 }
 
