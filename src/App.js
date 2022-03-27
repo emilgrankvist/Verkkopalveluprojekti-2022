2c31d41cc5deffaed4446eefa2606503b0bef124
Sami Kurvinen <n0kusa00@students.oamk.fi>
2022-03-27 11:35:41 +0300

    testi123

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tietokonekauppa</h1>
+      <div>
+        <header><a>Tietokoneet</a><a>Komponentit</a><a>Oheislaitteet</a></header>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
