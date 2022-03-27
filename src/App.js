e09ec5aa78080ff94e2fedb7b4703815165df89f
Sami Kurvinen <n0kusa00@students.oamk.fi>
2022-03-27 15:00:06 +0300

    Komponentteja ja sivuja.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
+import React from 'react';
+import logo from './images/logoboisJOMS.png';
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products'
 
 function App() {
   return (
+    <>
+    <Header />
+    <Navbar />
+    <div className='container'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
     <div className="App">
-      <h1>Tietokonekauppa</h1>
       <div>
-        <header><a>Tietokoneet</a><a>Komponentit</a><a>Oheislaitteet</a></header>
+        <img src={logo}></img>
       </div>
     </div>
-
+    <Footer />
+    </>
   );
 }
 
