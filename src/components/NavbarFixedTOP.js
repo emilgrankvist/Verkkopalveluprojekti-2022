aa50ee0bf536c1cd8c667605b5bc03a01c5d5ea0
Emil Grankvist <emil.grankvist@icloud.com>
2022-04-07 14:04:22 +0300

    etusivu muokkauksia

--- src/components/NavbarFixedTOP.js
+++ src/components/NavbarFixedTOP.js
@@ -5,18 +5,21 @@ import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function NavbarFixedTOP() {
     return (
-<div className='fixed-top FIXEDnavbar ms-auto ' >
-<Navbar/>
-<Container>
+<div className='fixed-top FIXEDnavbarTOP ms-auto ' >
+<Navbar className='fixed-top '/>
+<Container >
   <Navbar expand="lg" variant="light" bg="light">
-    <Container>
       <Navbar.Brand href="#"><FontAwesomeIcon icon={faHome} /></Navbar.Brand>
-    </Container>
-    <form className="d-flex navbarHAKU paddingLisäys">
+      <Navbar.Brand className='marginOikealle' href="#"><FontAwesomeIcon icon={faCartShopping} /></Navbar.Brand>
+    <form>
+      
+    </form>
+    <form className="d-flex navbarHAKU paddingLisäys HakuOikealle">
         <input className="form-control me-2 " type="search" placeholder="Hakusana" aria-label="Search"/>
         <button className="btn btn-outline-primary" type="submit">Etsi</button>
       </form>
